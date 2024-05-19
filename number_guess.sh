b73d483547865c0c653645167a6c462b9792482e
Davy Paulino <davy.paulino@aluno.ifsp.edu.br>
2024-05-19 16:46:10 +0000

    feat: Add user to the database and check if user exist

--- number_guess.sh
+++ number_guess.sh
@@ -1,2 +1,23 @@
 #!/bin/bash
 
+PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
+
+SECRET_NUMBER=$($RANDOM % 1000 + 1)
+
+echo -e "\n~~~~~ Number Guess Game ~~~~~\n"
+
+MAIN_MENU() {
+  echo -e "\nEnter your username:"
+  read USERNAME
+  USER_ID=$($PSQL "SELECT user_id FROM users WHERE username = '$USERNAME'")
+  if [[ -z $USER_ID ]]
+  then
+    INSERT_USER_RESULT=$($PSQL "INSERT INTO users(username) VALUES('$USERNAME')")
+    echo $INSERT_USER_RESULT
+  else
+    GAMES_INFO=$($PSQL "SELECT COUNT(*), MIN(number_of_guesses) FROM users RIGHT JOIN guess_games USING(user_id) WHERE user_id = $USER_ID")
+    echo $GAMES_INFO
+  fi
+}
+
+MAIN_MENU
